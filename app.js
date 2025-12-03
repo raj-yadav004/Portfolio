c3237759e10cc7f410df41ebaa27d4747716706f
raj yadav <rajyadav565422@gmail.com>
2025-12-04 00:13:00 +0530

    completed till myskills

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 let typed = new Typed("#profession", {
-    string: ["Web Developer", "Graphic Designer", "Frontend Developer"],
+    strings: ["Web Developer", "Graphic Designer", "Frontend Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 
+});
+const hamburger = document.querySelector(".hamburger-menu");
+const nav = document.querySelector("#nav");
+const navlinks = document.querySelectorAll('#nav a');
+
+hamburger.addEventListener('click',()=>{
+    nav.classList.toggle('active');
+    
+    const icon = hamburger.querySelector('i');
+    if(nav.classList.contains('active')){
+        icon.classList.remove('bx-menu-alt-left');
+        icon.classList.add('bx-x');
+    }
+    else {
+        icon.classList.remove('bx-x');
+        icon.classList.add('bx-menu-alt-left');
+    }
+});
+
+navlinks.forEach(link => {
+    
+    link.addEventListener("click",()=>{
+        nav.classList.remove('active'); 
+        const icon = hamburger.querySelector('i'); 
+         icon.classList.remove('bx-x');
+        icon.classList.add('bx-menu-alt-left');
+    });
+});
+
+document.addEventListener("click",(e)=>{
+    if(!nav.contains(e.target) && !hamburger.contains(e.target)){
+          nav.classList.remove('active'); 
+        const icon = hamburger.querySelector('i'); 
+         icon.classList.remove('bx-x');
+        icon.classList.add('bx-menu-alt-left');
+    }
 });
