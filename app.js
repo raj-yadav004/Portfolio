e1c36fec1027c132330bf51a3d658088214e04ca
raj yadav <rajyadav565422@gmail.com>
2025-12-03 20:54:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let typed = new Typed("#profession", {
+    string: ["Web Developer", "Graphic Designer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
